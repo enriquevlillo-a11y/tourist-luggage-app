7286346dcb15c816a6042b3804aa712a46546f5a
Kevin Pluas <kpluas21@gmail.com>
2025-10-12 14:19:35 -0400

    Refactor MapScreen to set user location as initial region and handle location permissions

--- luggage-frontend/components/map-screen.tsx
+++ luggage-frontend/components/map-screen.tsx
@@ -1,22 +1,24 @@
 //Our main map view component. This can be imported and placed on any page. 
 //TODO: Use the newly acquired Google Maps API Key | MAY NEED TO BE DEPLOYED TO GOOGLE APP STORE AT LEAST ONCE 
 //TODO: Communicate with backend to place pins of hosts
-//TODO: Change the default location to the users', preferably. 
+//TODO: Change the default location to the users', preferably.  UPDATE: Done but the android emulator defaults to Google HQ. On an actual device, it grabs the location but does NOT change the view to reflect this. 
 
 import React from 'react';
+import { Text } from '@react-navigation/elements';
 import { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Region } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 
 export default function MapScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [region, setRegion] = useState<Region | undefined>(undefined)
 
   useEffect(() => {
     async function getCurrentLocation() {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
@@ -25,6 +27,14 @@ export default function MapScreen() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05
+      })
     }
 
     getCurrentLocation();
@@ -36,18 +46,20 @@ export default function MapScreen() {
   } else if (location) {
     text = JSON.stringify(location);
   }
-  
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 25.7617,     // Miami
-          longitude: -80.1918,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={region}
+        showsUserLocation={true}
+        followsUserLocation={false}
       >
+        {region && (
+          <Marker
+            coordinate={region}
+          />
+        )}
         <Marker
           coordinate={{ latitude: 25.7617, longitude: -80.1918 }}
           title="Miami"
